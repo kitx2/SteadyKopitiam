77b620d9cca3facced74f792ce16aa670f56d63b
Young Kit <e0119427.u.nus.edu>
2019-11-06 15:28:52 +0800

    Fixed validation for loginActivity
    - Fixed validation for loginActivity
    - Fixed minor EditText to singline textbox

--- app/src/main/java/com/example/steadykopitiam/LoginActivity.kt
+++ app/src/main/java/com/example/steadykopitiam/LoginActivity.kt
@@ -5,8 +5,11 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import com.example.steadykopitiam.ui.home.HomeActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.nav_header_main.*
@@ -36,40 +39,47 @@ class LoginActivity : AppCompatActivity() {
             startActivity(myIntent)
         }
 
+        // error message
+        val errorCard : CardView = findViewById(R.id.errorCard)
+        errorCard.visibility = View.INVISIBLE
+        val errorMsg : TextView = findViewById(R.id.errorMsg)
+
         // to log in user
         val btn_login = findViewById<Button>(R.id.btn_login)
         btn_login.setOnClickListener{
-            var email = this.email_input_login.text.toString()
-            var password = this.email_input_password.text.toString()
+            val email = this.email_input_login.text.toString()
+            val password = this.email_input_password.text.toString()
 
-            var user = ArrayList<UserRecord>()
-            user = kopitiamDBHelper.readUser(email,password)
+            if(email_input_login.text.isEmpty() || email_input_password.text.isEmpty()) {
+                errorCard.visibility = View.VISIBLE
+                errorMsg.text = "Please enter the required field(s)"
+            } else {
 
-            if(user.isEmpty()){
-                Toast.makeText(this,"This User does not exists",Toast.LENGTH_LONG).show()
-            }else{
-                // -- to login to the main page -- //
-                userpassword = user.get(0).user_password
-                useremail = user.get(0).email
-                var sharedPrefEditor = sharedPreferences.edit()
-                sharedPrefEditor.putString("userPassword", userpassword)
-                sharedPrefEditor.putString("userEmail",useremail)
-                sharedPrefEditor.commit()
+                var user = ArrayList<UserRecord>()
+                user = kopitiamDBHelper.readUser(email, password)
 
-                val myIntent = Intent(this, HomeActivity::class.java)
-                startActivity(myIntent)
-                finish()
+                if (user.isEmpty()) {
+                    errorCard.visibility = View.VISIBLE
+                    errorMsg.text = "Wrong email or password."
+                } else {
+                    // -- to login to the main page -- //
+                    errorCard.visibility = View.INVISIBLE
+                    userpassword = user.get(0).user_password
+                    useremail = user.get(0).email
+                    var sharedPrefEditor = sharedPreferences.edit()
+                    sharedPrefEditor.putString("userPassword", userpassword)
+                    sharedPrefEditor.putString("userEmail", useremail)
+                    sharedPrefEditor.commit()
+
+                    val myIntent = Intent(this, HomeActivity::class.java)
+                    startActivity(myIntent)
+                    finish()
+                }
             }
 
         }
 
-
         val f1 = resources.getStringArray(R.array.food1)
-
-
-
-
-
 
     }
 }
